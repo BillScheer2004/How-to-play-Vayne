d18b98d54f73bbb1533ca43c8b78daa237ab1975
BillScheer2004 <79986424+BillScheer2004@users.noreply.github.com>
2022-05-16 14:24:13 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,7 @@
+function tn() {
+    document.getElementById('Vaynebild').style.visibility='hidden';
+  }
+
+  function tend() {
+    document.getElementById('Vaynebild').style.visibility='visible';
+  }
